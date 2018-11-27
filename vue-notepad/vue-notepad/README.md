4188d435c6a12e6f131ab949670573633df09df4
LiuYao <liuyao@vyying.com>
2018-11-27 19:07:19 +0800

    vue2.0+vuex+localStorage+sass+webpack，实现一个本地存储的记事本

---
+++ vue-notepad/vue-notepad/README.md
@@ -0,0 +1,5 @@
+# vue-notepad
+
+>1.cnpm install -D node-sass sass-loader style-loader
+2.cnpm install --save vuex
+
---
+++ vue-notepad/vue-notepad/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app"
+       :class="[getTheme]">
+    <n-header></n-header>
+  </div>
+</template>
+
+<script>
+import nHeader from './components/header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    nHeader
+  },
+  computed: {
+    getTheme () {             // 获取主题色
+      return 'blue'
+      //this.$store.getters.getTheme;
+    }
+  }
+}
+</script>
+<style lang="scss" rel="stylesheet/scss" src="../static/theme.scss"></style>
+<style lang="scss">
+</style>
